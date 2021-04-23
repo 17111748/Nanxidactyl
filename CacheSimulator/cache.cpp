c0931e1d9160e98a6c657b9fba8163f5594c4d83
Albert Chen <albertc1@ece001.ece.local.cmu.edu>
2021-04-23 17:40:27 -0400

    Temporary edits... please ignore

--- CacheSimulator/cache.cpp
+++ CacheSimulator/cache.cpp
@@ -1,3 +1,103 @@
+// #include "cache.h"
+// #include "vector"
+// #include <cstdint>
+// #include <map>
+// #include <cmath>
+// #include <iostream>
+
+
+// // Buffer Sizes 
+// #define NUM_BLOCKS 4
+// #define BLOCK_SIZE 2 // Got to be log2(NUM_BLOCKS)
+// #define ADDR_SIZE 64
+
+// // L1 Cache parameters 
+// #define L1_SET_ASSOCIATIVITY 8
+// #define L1_NUM_SETS 32
+// // LLC Cache parameters
+// #define LLC_SET_ASSOCIATIVITY 8
+// #define LLC_NUM_SETS 32
+
+// using namespace std; 
+
+// Line::Line() {
+//     state = INVALID; 
+//     tag = 0;
+//     data = vector<uint32_t>(NUM_BLOCKS, 0); 
+//     time_accessed = 0;  
+// }
+
+// // Constructor
+// Line::Line(cache_states state_param, uint64_t tag_param, uint32_t data_param, uint64_t time_accessed_param, uint8_t block_index_param) {
+//     state = state_param;
+//     tag = tag_param;
+//     data = vector<uint32_t>(NUM_BLOCKS, 0); 
+//     data[block_index_param] = data_param;
+//     time_accessed = time_accessed_param; 
+// }
+
+// Set::Set() {
+//     setID = 0; 
+// }
+
+// // Constructor
+// Set::Set(std::vector<Line> lines_param, uint64_t num_lines_param, uint8_t setID_param) {
+//     lines = lines_param;	
+//     num_lines = num_lines_param; 
+//     setID = setID_param;
+// }
+
+// // Constructor 
+// Cache_stat::Cache_stat() {
+//     num_access = 0; 
+//     num_reads_hits = 0; 
+//     num_writes_hits = 0; 
+//     num_read_misses = 0; 
+//     num_write_misses = 0;
+//     num_read_from_llc = 0; // read: line not in cache or no M/S version in other caches
+//     num_write_to_llc = 0;
+// }
+
+// Cache::Cache() {
+//     num_sets = 0; 
+// }
+
+// // Constructor
+// Cache::Cache(uint64_t set_associativity_param, uint64_t num_sets_param) {
+
+//     cache_stats = Cache_stat(); 
+//     sets = map<uint64_t, Set> {};// Map of index -> Set 
+//     for (uint64_t setID = 0; setID < num_sets_param; setID++) {
+//         // vector<Line> lines(set_associativity_param, Line());
+//         vector<Line> lines;
+//         uint64_t num_lines = set_associativity_param;
+//         Set s = Set(lines, num_lines, setID);
+//         sets.insert(pair<uint64_t, Set>(setID, s));    
+//     }
+
+//     // std::cout << "in cache " << unsigned(set_associativity) << endl; 
+//     set_associativity = set_associativity_param;
+//     num_sets = num_sets_param; 
+
+// }
+
+// // TODO: CHECK THESE FUNCTIONS 
+// // Cache Helper Functions 
+// std::vector<uint64_t> Cache::address_convert(uint64_t addr) {
+//     uint8_t index_length = log2(num_sets); 
+//     uint8_t tag_length = ADDR_SIZE - BLOCK_SIZE - index_length; 
+
+//     uint64_t tag = addr >> (ADDR_SIZE - tag_length); 
+//     uint64_t index = (addr << tag_length) >> (tag_length + BLOCK_SIZE); 
+//     uint64_t block = addr << (ADDR_SIZE - BLOCK_SIZE) >> (ADDR_SIZE = BLOCK_SIZE);
+
+//     std::vector<uint64_t> result; 
+//     result.push_back(tag); 
+//     result.push_back(index); 
+//     result.push_back(block); 
+//     return result; 
+// }
+
 #include "cache.h"
 #include "vector"
 #include <cstdint>
@@ -93,7 +193,6 @@ std::vector<uint64_t> Cache::address_convert(uint64_t addr) {
     result.push_back(index); 
     return result; 
 }
-
 
 
 
